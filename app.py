94b64ac1c28e7af4687f4315bc459570b605c468
mustufa-ahmed <mustafa.ahmed@salsoft.net>
2022-09-04 18:31:02 +0500

    add encoding

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def main():
     st.title("SMS Data Analysis")
-    data = pd.read_csv("SMS_data.csv")
+    data = pd.read_csv("SMS_data.csv",encoding='unicode_escape')
 
 if __name__ == '__main__':
     main()
