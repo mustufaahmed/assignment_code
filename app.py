d6622294b9bee87bca2c4f53014c2ee5d07712a7
mustufa-ahmed <mustafa.ahmed@salsoft.net>
2022-09-04 19:40:03 +0500

    fix

--- app.py
+++ app.py
@@ -26,13 +26,16 @@ def main():
     # Get Data Where Only Spam Messages
     spam_messages = getSpamData(data)
     # Get Frequency of spam messages according to months
-    print(getBarChartPlot(spam_messages))
+    # print(getBarChartPlot(spam_messages))
+    data = [[30, 25, 50, 20],
+    [40, 23, 51, 17],
+    [35, 22, 45, 19]]
+    X = np.arange(4)
     fig = plt.figure()
     ax = fig.add_axes([0,0,1,1])
-    langs = ['C', 'C++', 'Java', 'Python', 'PHP']
-    students = [23,17,35,29,12]
-    ax.bar(langs,students)
-    plt.show()
+    ax.bar(X + 0.00, data[0], color = 'b', width = 0.25)
+    ax.bar(X + 0.25, data[1], color = 'g', width = 0.25)
+    ax.bar(X + 0.50, data[2], color = 'r', width = 0.25)
 
 if __name__ == '__main__':
     main()
